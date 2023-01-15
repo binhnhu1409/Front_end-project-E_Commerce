871e816885b80ac098eec46b7dbb36e3315b24ef
Nhu Nguyen Tran Binh <binhnhu1409@gmail.com>
2023-01-15 06:39:35 +0200

    fetchAllCategories: UI done

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,6 +1,32 @@
+import { useEffect } from "react"
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHook"
+
+import { fetchAllCategories } from "../redux/reducers/categoryReducer"
+import "../SASS/component/categories.scss"
+
 const Categories = () => {
+  const dispatch = useAppDispatch()
+  const categories = useAppSelector(state => state.categoryReducer)
+  console.log ("categories", categories)
+  useEffect(() => {
+    dispatch(fetchAllCategories())
+  }, [dispatch])
+
   return (
-    <div>Categories</div>
+    <div>
+      {!categories ? (
+        <h2>Loading...</h2>
+      ) : (
+      <div className="categories">
+        {categories.map(item => (
+          <div className="categories__card">
+            <span className="categories__name">{item.name}</span>
+            <img className="categories__image" src={item.image} alt="" />
+          </div>
+        ))}
+      </div>
+      )}
+    </div>
   )
 }
 
